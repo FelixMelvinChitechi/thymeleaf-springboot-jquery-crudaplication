bd3abaae39bd436bb860bd1524041e443fa59e9e
Vinn <newuser@localhost.localdomain>
2022-07-14 01:31:40 +0300

    first commit

---
+++ src/main/java/com/luv2code/springbootwebapp/entity/Employee.java
@@ -0,0 +1,78 @@
+package com.luv2code.springbootwebapp.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tbl_employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String department;
+	
+	public Employee() {
+		
+	}
+
+	public Employee(String name, String email, String department) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.department = department;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", department=" + department + "]";
+	}
+	
+}
